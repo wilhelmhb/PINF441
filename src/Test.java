5f3348f0dfb39118214d0041516cc73229396dc8
wilhelmhb <wilhelmhb@live.fr>
2016-04-27 20:09:35 +0200

    lancer plusieurs morpions de tailles différentes à la suite

--- src/Test.java
+++ src/Test.java
@@ -7,18 +7,22 @@ import java.util.logging.Level;
  */
 public class Test{
 	public static void main(String[] args) {
-		test();
+		for(int l = 0; l < 10; l++) {
+			testOnce(l, (int) Math.floor(3 + l * 1.5), (int) Math.floor(3 + l* 1.51), (int) Math.floor(3 + l* 1.4));
+		}
 	}
 	
 	/**
 	  create a picross, solve it and display it
 	  @author Guillaume
 	 */
-	public static void test() {
-		Morpion p = new Morpion(3,3,3);
-		Window f = new Window(p, "Test");
+	public static void testOnce(int l, int i, int j, int k) {
+		Morpion p = new Morpion(i, j, k);
+		Window f = new Window(p, "Test" + l);
+		p.setLevelLogger(Level.WARNING);
 		p.play();
-		//try {Thread.sleep(1000);}catch(Exception e){}
+		try {Thread.sleep(1000);}catch(Exception e){}
+		return;
 	}
 	
 	/**
