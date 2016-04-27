9248753d3afc7b03659d646ae4424d91b161930d
wilhelmhb <wilhelmhb@live.fr>
2016-04-27 17:54:52 +0200

    ajout d'une classe chapeau pour lancer facilement un ou plusieurs morpions

---
+++ src/Test.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+  class to launch the picross and the solver
+  @author guillaume
+ 
+ */
+public class Test{
+	public static void main(String[] args) {
+		test();
+	}
+	
+	/**
+	  create a picross, solve it and display it
+	  @author Guillaume
+	 */
+	public static void test() {
+		Morpion p = new Morpion(3,3,3);
+		Window f = new Window(p, "Test");
+		p.play();
+		//try {Thread.sleep(1000);}catch(Exception e){}
+	}
+	
+	/**
+	  create a picrossColored, solve it and display it
+	  @param file
+	  @author Guillaume
+	 */
+	/*public static void testColored(String file) {
+		MorpionColored p = new MorpionColored(new File(file).getAbsolutePath());
+		Window f = new Window(p, file);
+		p.solveOpt();
+		//try {Thread.sleep(1000);}catch(Exception e){}
+	}*/
+}
