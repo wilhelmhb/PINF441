969c6d2ce60def3225526100dee973d07ee5b5ba
wilhelmhb <wilhelmhb@live.fr>
2016-05-05 11:43:42 +0200

    modification de l'interface de Position

--- src/PositionInterface.java
+++ src/PositionInterface.java
@@ -3,7 +3,7 @@ import structures.Pair;
 /**
  * Created by wilhelm on 04/05/16.
  */
-public interface PositionInterface {
+interface PositionInterface<Action> {
     Boolean getPlayer();
 
     Boolean[][] getState();
@@ -12,11 +12,64 @@ public interface PositionInterface {
 
     boolean isFull();
 
-    PositionInterface getResult(Position position, Pair<Integer, Integer> action);
-
-    Boolean getCell(Integer column, Integer row);
+    //Boolean getCell(Integer column, Integer row);
 
     Integer getUtility();
 
-    void computeUtility();
+    Pair<Pair<Integer,Integer>,Integer> computeUtilityAndCellsLeftAndPlayer();
+
+    Position getResult(Position position, Action action);
+
+    boolean equals(Position e);
+
+    boolean sameAs(Position e);
+
+    boolean sameStateAs(Boolean[][] state);
+
+    /**
+     * test if player won the game thanks to a row
+     * @param player
+     * @param column
+     * @param row
+     * @return true if thanks to a move in cell (column, row), player won thanks to a row
+     */
+    boolean isWonHorizontally(boolean player, Integer column, Integer row);
+
+    /**
+     * test if player won the game thanks to a column
+     * @param player
+     * @param column
+     * @param row
+     * @return true if thanks to a move in cell (column, row), player won thanks to a column
+     */
+    boolean isWonVertically(boolean player, Integer column, Integer row);
+
+    /**
+     * test if player won the game thanks to the first bisector
+     * @param player
+     * @param column
+     * @param row
+     * @return true if thanks to a move in cell (column, row), player won thanks to the first bisector
+     */
+    boolean isWonOnFirstDiag(boolean player, Integer column, Integer row);
+
+    /**
+     * test if player won the game thanks to a second bisector
+     * @param player
+     * @param column
+     * @param row
+     * @return true if thanks to a move in cell (column, row), player won thanks to the second bisector
+     */
+    boolean isWonOnSecondDiag(boolean player, Integer column, Integer row);
+
+    /**
+     * test if player won
+     * @param player
+     * @param column
+     * @param row
+     * @return true if player won thanks to a piece in cell (column, row)
+     */
+    boolean isWon(boolean player, Integer column, Integer row);
+
+    boolean isTerminal();
 }
