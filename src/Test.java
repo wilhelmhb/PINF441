d5e676554728f83960b5d0c918fec9c116837895
wilhelmhb <wilhelmhb@live.fr>
2016-05-27 00:53:29 +0200

    ajout d'une fenêtre pour Connect4, et de tests

--- src/Test.java
+++ src/Test.java
@@ -14,14 +14,18 @@ public class Test{
 			testOnceNegaMax(l, (int) Math.floor(3 + l * 1.5), (int) Math.floor(3 + l * 2), (int) Math.floor(3));
 			testOnceAlphaBeta(l, (int) Math.floor(3 + l * 1.5), (int) Math.floor(3 + l * 2), (int) Math.floor(3));
 		}*/
-		/*testOnceRandomly(1, 3, 3, 3);
-		testOnceMiniMax(1, 3, 3, 3);
-		testOnceNegaMax(1, 3, 3, 3);
-		testOnceAlphaBeta(1, 3, 3, 3);*/
-		testOnceRandomly(1, 4, 3, 3);
-		testOnceMiniMax(1, 4, 3, 3);
-		testOnceNegaMax(1, 4, 3, 3);
-		testOnceAlphaBeta(1, 4, 3, 3);
+		/*testOnceRandomly("1", 3, 3, 3);
+		testOnceMiniMax("2", 3, 3, 3);
+		testOnceNegaMax("3", 3, 3, 3);*/
+		testOnceAlphaBeta("4", 3, 3, 3);
+		/*testOnceRandomly("1.1", 3, 4, 3);
+		//testOnceMiniMax("2.1", 3, 4, 3);
+		//testOnceNegaMax("3.1", 3, 4, 3);
+		testOnceAlphaBeta("4.1", 4, 4, 3);
+		testOnceRandomly("1.2", 4, 4, 3);
+		//testOnceMiniMax("2.2", 4, 4, 3);
+		testOnceNegaMax("3.2", 4, 4, 3);
+		testOnceAlphaBeta("4.2", 4, 4, 3);*/
 		return;
 	}
 	
@@ -32,38 +36,60 @@ public class Test{
 	 * @param j : number of rows
      * @param k : aim of the morpion
      */
-	public static void testOnceRandomly(int l, int i, int j, int k) {
-		Tictactoe p = new Tictactoe(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
+	public static void testOnceRandomly(String l, int i, int j, int k) {
+		/*Tictactoe p = new Tictactoe(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
 		Window f = new Window(p, "Test" + l + ", dimensions : (" + i + "x" + j + "), but : " + k, j * (50 + 10), i * (50 + 10));
 		p.setLevelLogger(Level.FINE);
 		p.playRandomly();
+		try {Thread.sleep(1000);}catch(Exception e){}*/
+		Connect4 c = new Connect4(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
+		Window w = new Window(c, "Connect4" + l + ", dimensions : (" + i + "x" + j + "), but : " + k, j * (50 + 10), i * (50 + 10));
+		c.setLevelLogger(Level.FINE);
+		c.play();
 		try {Thread.sleep(1000);}catch(Exception e){}
 		return;
 	}
 
-	public static void testOnceMiniMax(int l, int i, int j, int k) {
-		Tictactoe p = new Tictactoe(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
+	public static void testOnceMiniMax(String l, int i, int j, int k) {
+		/*Tictactoe p = new Tictactoe(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
 		Window f = new Window(p, "Test" + l + ", dimensions : (" + i + "x" + j + "), but : " + k, j * (50 + 10), i * (50 + 10));
 		p.setLevelLogger(Level.FINE);
 		p.playMiniMax();
+		try {Thread.sleep(1000);}catch(Exception e){}*/
+		Connect4 c = new Connect4(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
+		Window w = new Window(c, "Connect4" + l + ", dimensions : (" + i + "x" + j + "), but : " + k, j * (50 + 10), i * (50 + 10));
+		c.setLevelLogger(Level.FINE);
+		c.playMiniMax();
 		try {Thread.sleep(1000);}catch(Exception e){}
 		return;
 	}
 
-	public static void testOnceNegaMax(int l, int i, int j, int k) {
-		Tictactoe p = new Tictactoe(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
+	public static void testOnceNegaMax(String l, int i, int j, int k) {
+		/*Tictactoe p = new Tictactoe(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
 		Window f = new Window(p, "Test" + l + ", dimensions : (" + i + "x" + j + "), but : " + k, j * (50 + 10), i * (50 + 10));
 		p.setLevelLogger(Level.FINE);
 		p.playNegaMax();
+		try {Thread.sleep(1000);}catch(Exception e){}*/
+		Connect4 c = new Connect4(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
+		Window w = new Window(c, "Connect4" + l + ", dimensions : (" + i + "x" + j + "), but : " + k, j * (50 + 10), i * (50 + 10));
+		c.setLevelLogger(Level.FINE);
+		c.playNegaMax();
 		try {Thread.sleep(1000);}catch(Exception e){}
 		return;
 	}
 
-	public static void testOnceAlphaBeta(int l, int i, int j, int k) {
-		Tictactoe p = new Tictactoe(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
+	public static void testOnceAlphaBeta(String l, int i, int j, int k) {
+		/*Tictactoe p = new Tictactoe(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
 		Window f = new Window(p, "Test" + l + ", dimensions : (" + i + "x" + j + "), but : " + k, j * (50 + 10), i * (50 + 10));
 		p.setLevelLogger(Level.FINE);
 		p.playAlphaBeta();
+		try {Thread.sleep(1000);}catch(Exception e){}*/
+		Connect4 c = new Connect4(i, j, k, 50, 50, Color.BLUE, Color.GREEN);
+		Window w = new Window(c, "Connect4" + l + ", dimensions : (" + i + "x" + j + "), but : " + k, j * (50 + 10), i * (50 + 10));
+		c.setLevelLogger(Level.FINE);
+		c.move(true, 0);
+		c.setPlayer(false);
+		c.playAlphaBeta();
 		try {Thread.sleep(1000);}catch(Exception e){}
 		return;
 	}
